5b07ab00cb565f5d8a029e9a578b28aa0791c129
dev <dev@local>
2026-03-27 11:29:47 +0800

    Set dev DB password default for backend runner

--- scripts/run_backend_dev.ps1
+++ scripts/run_backend_dev.ps1
@@ -1,3 +1,4 @@
 $env:SPRING_PROFILES_ACTIVE = 'dev'
+$env:DB_PASSWORD = if ($env:DB_PASSWORD) { $env:DB_PASSWORD } else { '123456' }
 Set-Location 'g:\myapps\AI_Ds'
 & 'G:\downloads\apache-maven-3.9.9\bin\mvn.cmd' -q -DskipTests spring-boot:run
